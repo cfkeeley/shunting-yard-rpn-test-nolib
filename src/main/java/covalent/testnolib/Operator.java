385a46cc267d2c2b8e5794de60530fd4cb61a7b5
Chris Keeley <chris@relationshapes.com>
2014-02-17 21:39:56 +0000

    Initial implementation of the shunting yard algorithm to parse out the expression into reverse polish notation from its original infix format.

---
+++ src/main/java/covalent/testnolib/Operator.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package covalent.testnolib;
+
+/**
+ * @author chris
+ * Set of operators 
+ */
+public enum Operator {
+	
+	PLUS("+",1),MINUS("-",1),MULTIPLY("*",2),DIVIDE("/",2);
+	
+	private String symbol;
+	private int precedence;
+	
+	Operator(String aSymbol, int aPrecedence) {
+		symbol = aSymbol;
+		precedence = aPrecedence;
+	}
+	
+	/**
+	 * @return the symbol
+	 */
+	public String getSymbol() {
+		return symbol;
+	}
+	
+	/**
+	 * Lookup an instance of Operator using a string symbol
+	 * @param symbol
+	 * @return
+	 */
+	public static Operator instanceOf(String symbol) {
+		for(Operator op : values()) {
+			if(op.getSymbol().equalsIgnoreCase(symbol)) {
+				return op;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Lookup an instance of Operator using a string symbol
+	 * @param symbol
+	 * @return
+	 */
+	public static boolean isOperator(String symbol) {
+		for(Operator op : values()) {
+			if(op.getSymbol().equalsIgnoreCase(symbol)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * @return the precedence
+	 */
+	public int getPrecedence() {
+		return precedence;
+	}
+}
